e53cac7932e70ef59b12963945f4e1c2b2d21c31
kiril-buga <kirilbuga@yahoo.com>
2022-01-17 18:09:41 +0100

    Frontend files

---
+++ BestBook Frontend/assets/js/script_contactus.js
@@ -0,0 +1,60 @@
+//Get data
+const nameInput1 = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const nameInput = document.getElementById("name");
+const success = document.getElementById("success");
+const errorNodes = document.getElementsByClassName("error");
+
+//Validate data
+function validateForm(){
+
+    clearMessages();
+    let errorFlag = false;
+
+    if(nameInput1.value.length < 1){
+        errorNodes[0].innerText = "Name cannot be blank";
+        nameInput1.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(subject.value.length < 1){
+        errorNodes[1].innerText = "Please enter a subject of your concern";
+        subject.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(!emailIsValid(email.value)){
+        errorNodes[2].innerText = "Invalid email address";
+        email.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(message.value.length < 1){
+        errorNodes[3].innerText = "Please enter message";
+        email.classList.add("error-border");
+        errorFlag = true;
+    }
+
+    if(!errorFlag){
+        success.innerText = "Your message has been sent!";
+    }
+}
+
+// Clear error / success messages
+function clearMessages(){
+    for(let i = 0; i < errorNodes.length; i++){
+        errorNodes[i].innerText = "";
+    }
+    success.innerText = "";
+    nameInput1.classList.remove("error-border");
+    email.classList.remove("error-border");
+    message.classList.remove("error-border");
+}
+
+//check if email is valid
+function emailIsValid(email){
+    let pattern = /\S+@\S+\.\S+/;
+    return pattern.test(email);
+}
---
+++ BestBook Frontend/assets/js/theme.js
@@ -0,0 +1,18 @@
+
+  // Custom theme code
+
+if (document.getElementsByClassName('clean-gallery').length > 0) {
+    baguetteBox.run('.clean-gallery', { animation: 'slideIn' });
+ }
+ 
+ if (document.getElementsByClassName('clean-product').length > 0) {
+     window.onload = function() {
+         vanillaZoom.init('#product-preview');
+     };
+ }
+ 
+ if (document.readyState == 'loading') {
+     document.addEventListener('DOMContentLoaded', ready)
+ } else {
+     ready()
+ }
