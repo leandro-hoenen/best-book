a6e4c4c1d49d59158d4af445647b7d71dc51f592
leandro-hoenen <leandro.hoenen@gmail.com>
2022-01-18 19:17:52 +0100

    edit videoGame implemented

--- src/main/java/ch/fhnw/acrm/data/repository/VideoGameRepository.java
+++ src/main/java/ch/fhnw/acrm/data/repository/VideoGameRepository.java
@@ -1,5 +1,6 @@
 package ch.fhnw.acrm.data.repository;
 
+import ch.fhnw.acrm.data.domain.Agent;
 import ch.fhnw.acrm.data.domain.Customer;
 import ch.fhnw.acrm.data.domain.VideoGame;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import java.util.List;
 public interface VideoGameRepository extends JpaRepository<VideoGame, Long> {
     List<VideoGame> findByAgentId(Long agentId);
     List<VideoGame> findByIdAndAgentId(Long videoGameId, Long agentId);
+
 }
