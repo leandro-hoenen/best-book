a671207c0a15d82e3272b085de0b77b29f5f401c
leandro-hoenen <leandro.hoenen@gmail.com>
2022-01-18 17:58:37 +0100

    implemented id check for enterMovie method.

--- src/main/java/ch/fhnw/acrm/business/service/MovieService.java
+++ src/main/java/ch/fhnw/acrm/business/service/MovieService.java
@@ -1,10 +1,12 @@
 package ch.fhnw.acrm.business.service;
 
+import ch.fhnw.acrm.data.domain.Book;
 import ch.fhnw.acrm.data.domain.Movie;
 import ch.fhnw.acrm.data.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Service
@@ -15,9 +17,12 @@ public class MovieService {
     @Autowired
     private AgentService agentService;
 
-    public Movie enterMovie(Movie movie){
-        movie.setAgent(agentService.getCurrentAgent());
-        return movieRepository.save(movie);
+    public Movie enterMovie(@Valid Movie movie) throws Exception {
+        if (movie.getId() == null) {
+            movie.setAgent(agentService.getCurrentAgent());
+            return movieRepository.save(movie);
+        }
+        throw new Exception("Movie object already exists");
     }
 
     public List<Movie> myMovies(){
